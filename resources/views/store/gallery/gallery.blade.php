f77dd61a2190f3e61dcd8fd1d9d3724c975c1413
danielDev <valcyum.085@gmail.com>
2019-09-20 10:29:41 -0400

    arreglos en vista procedimientos y cambios menores

--- resources/views/store/gallery/gallery.blade.php
+++ resources/views/store/gallery/gallery.blade.php
@@ -5,54 +5,52 @@
 
 <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
     <!-- Our Services Area -->
-    <section class="our_services_area" style="padding-bottom: 300px">
+    <section class="our_services_area">
         <div class="container">
-            <div class=" text-inverse portfolio_inner_area col-md-12">
+            @if(  (isset($galleries)) )
+                <div class=" text-inverse portfolio_inner_area col-md-12">
                     <div class="portfolio_filter" >
-                        {{--<ul>--}}
-                          @foreach ($galleries as $gallery)      
-                                              
-                             {{--<li data-filter=".adversting">--}}
-                                 {{--<div class="row">--}}
-                                     {{--<div class="col-xs-12 col-md-4 col-sm-4">--}}
-
-                                         <div class="col-sm-4 col-xs-12 col-sm-4 zoom">
-                                             <img style="padding: 10px;" height="250" src="{{ asset( ($gallery->photos)[0]->path )  }}" alt="" >
-                                             <p class="text-center text-black">
-                                                 <i class="fa fa-folder-open" aria-hidden="true"></i>
-                                                 <a href="{{route('gallery/photo',$gallery->id)}}" style="color: #222222"><b>{{ $gallery->title }} </b> </a>
-                                             </p>
-                                         </div>
-
-                                     {{--</div>--}}
-
-                                 {{--</div>--}}
-
-                            {{--</li>  --}}
-                          @endforeach
-                        {{--</ul>--}}
-                    </div>
-                @if($pho ==1)
-                <div class="tittle wow fadeInUp" >            
-                   <h4>{{ $galerias->content }}</h4>
-                </div>
-                <br>
-
-
-                <div class="portfolio_item">
-                   <div class="grid-sizer"></div>
-                    @foreach ($galerias->photos as $phot)
-                    <div class="single_facilities col-md-4 col-xs-12 p0 adversting webdesign adversting">
-                       <div class="single_facilities_inner">
-                            <img style="padding: 10px;" src="{{ asset($phot->path) }}" alt="" height="400" width="400" >
-                         
+                        <div class="row">
+                            @foreach ($galleries as $gallery)
+                                <div class="col-sm-4 col-xs-12 col-sm-4 zoom">
+                                    <img style="padding: 10px;" height="250" src="{{ asset( ($gallery->photos)[0]->path )  }}" alt="" >
+                                    <p class="text-center text-black">
+                                        <i class="fa fa-folder-open" aria-hidden="true"></i>
+                                        <a href="{{route('gallery/photo',$gallery->id)}}" style="color: #222222"><b>{{ $gallery->title }} </b> </a>
+                                    </p>
+                                </div>
+                            @endforeach
                         </div>
                     </div>
-                    @endforeach
-                </div>
+                    @if($pho ==1)
+                        <div class="row">
+                            <div class="tittle wow fadeInUp" >
+                                <h2> {{$galerias->title}}</h2>
+                                <h5>{{ $galerias->content }}</h5>
+                            </div>
+                            <br>
+                            <div class="portfolio_item">
+                                <div class="grid-sizer"></div>
+                                @foreach ($galerias->photos as $phot)
+                                    <div class="single_facilities col-md-4 col-xs-12 p0 adversting webdesign adversting">
+                                        <div class="single_facilities_inner">
+                                            <img style="padding: 10px;" src="{{ asset($phot->path) }}" alt="" height="400" width="400" >
+                                        </div>
+                                    </div>
+                                @endforeach
+                            </div>
+                        </div>
+                    @endif
+                 </div>
             @endif
-            </div>
+                @if( !isset($galleries))
+                <div class="row" style="padding-top: 50px">
+                    <span> <b> -- No hay Elementos en La Galería</b></span>
+                </div>
+                @endif
         </div>
+
+
     </section>
     <!-- End Our Services Area -->
     @stop
--- resources/views/store/normas.blade.php
+++ resources/views/store/normas.blade.php
@@ -2,25 +2,25 @@
 
 @section('content')
     <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
-    <section class="our_services_area">
+    <section class="our_services_area" style="padding-top: 10px; padding-bottom: 10px; background: rgba(0,0,0,.9);">
         <div class="container">
-            <div class="row">
+            <div class="row" style="background-color: rgba(255,255,255,1);">
 
-                <div class="col-md-6 col-sm-6 footer_about quick">
-                    <h2>Normas de la carrera</h2>
+                <div class="col-md-6 col-sm-6">
+                    <h2 style="padding-bottom: 10px; padding-top: 20px">Normas de la carrera</h2>
                     @foreach ($normas as $norma)
-                    <ul class="quick_link">
+                    <ul class="text-blue">
                        {{--<li><a href="{{ asset('/file/getNorma/'.$norma->url_document)}}"><i class="fa fa-chevron-right"></i>{{ $norma->nombre }}</a></li>--}}
-                       <li><a href="https://www.uagrm.edu.bo/carrera.php?codigo=64" target="_blank"><i class="fa fa-chevron-right"></i>{{ $norma->nombre }}</a></li>
+                       <li style="list-style: none"><a  href="https://www.uagrm.edu.bo/carrera.php?codigo=64" target="_blank"><i class="fa fa-chevron-right"></i> <b> {{ $norma->nombre }}</b></a></li>
                     </ul>
                     @endforeach
                 </div>
-                <div class="col-md-9 col-sm-9 footer_about">
-                    <h2>Normas de la universidad</h2>
+                <div class="col-md-9 col-sm-9">
+                    <h2 style="padding-bottom: 10px;" class="text-capitalize">Normas de la universidad</h2>
                      @foreach ($extermas as $norm)
-                    <ul class="quick_link">
+                    <ul class="text-capitalize">
                        {{--<li><a href="{{ asset('/file/getNorma/'.$norm->url_document)}}"><i class="fa fa-chevron-right"></i>{{ $norm->nombre }}</a></li>--}}
-                        <li><a href="https://www.uagrm.edu.bo/info/legislacion.php" target="_blank"><i class="fa fa-chevron-right"></i>{{ $norm->nombre }} </a></li>
+                        <li style="list-style: none"><a  href="https://www.uagrm.edu.bo/info/legislacion.php" target="_blank"><i class="fa fa-chevron-right"></i> <b> {{ $norm->nombre }} </b></a></li>
                     </ul>
                     @endforeach
                 </div>
