a019f05c0ffa6ec4420e1e0fbf05f7a93a802a38
danielDev <valcyum.085@gmail.com>
2019-11-07 14:35:37 -0400

    añadiendo valicacion a los graficos, cuando se encuentre vacio

--- resources/views/admin/report/estudiantes.blade.php
+++ resources/views/admin/report/estudiantes.blade.php
@@ -3,11 +3,15 @@
 @section('content')
     <div class="box box-primary">
         {{--<div class="col-sm-12 col-md-12 col-xs-12">--}}
+        @if($students_total!= null)
             <h1>Total de Alumnos </h1>
             <canvas id="grafico"  height="80px"></canvas>
             <canvas id="graficoLineas"  height="80px"></canvas>
-        {{--</div>--}}
-
+        @endif
+        @if($students_dates==null)
+            <h1>Total de Alumnos </h1>
+            <div class="alert alert-info"> No se Añadieron datos</div>
+        @endif
     </div>
 @endsection
 @section('js')
@@ -18,33 +22,35 @@
         //esta es la data, podemos pasarle variables directamente desde el backend usando blade de la siguiente forma { { $dato1 } },
         //seleccionamos el color de fondo para cada dato que le enviamos
         //añadimos las etiquetas correspondientes a la data
-        var primer_grafico = document.getElementById('grafico').getContext('2d');//seleccionamos el canvas
         //le pasamos como opcion adicional que sea responsivo
-        var mi_primer_grafico ={
-            type:"doughnut",
-            data:{
-                datasets:[{
-                    data: {{$students_in}},
-                    backgroundColor: [
-                        "#04B404","#FFBF00","#161990","#ea0d2c",
-                    ],
-                }],
-                labels:  {{ $students_dates }},
-            },
-            options:{
-                responsive: true,
-                title: {
-                    display: true,
-                    text: 'Total Alumnos'
-                }
-            },
-        };
-        // var myLineChart = new Chart(ctx, {
-        //     type: 'line',
-        //     data: data,
-        //     options: options
-        // });
-        window.pie = new Chart(primer_grafico,mi_primer_grafico);//le pasamos el grafico y la data para representarlo
+        if({{ $students_total }}!= null || {{ $students_total  }}!= undefined){
+        var primer_grafico = document.getElementById('grafico').getContext('2d');//seleccionamos el canvas
+            var mi_primer_grafico ={
+                type:"doughnut",
+                data:{
+                    datasets:[{
+                        data: {{$students_in}},
+                        backgroundColor: [
+                            "#04B404","#FFBF00","#161990","#ea0d2c",
+                        ],
+                    }],
+                    labels:  {{ $students_dates }},
+                },
+                options:{
+                    responsive: true,
+                    title: {
+                        display: true,
+                        text: 'Total Alumnos'
+                    }
+                },
+            };
+            // var myLineChart = new Chart(ctx, {
+            //     type: 'line',
+            //     data: data,
+            //     options: options
+            // });
+            window.pie = new Chart(primer_grafico,mi_primer_grafico);//le pasamos el grafico y la data para representarlo
+        }
     </script>
 
     <script>
@@ -60,33 +66,35 @@
             }
         };
 
+    if( {{$students_total }} != null  || {{ $students_total  }} != undefined){
+            var speedCanvas = document.getElementById("graficoLineas");
+            var speedData = {
+                labels: {{ $students_dates }},
+                datasets: [{
+                    label: "Ingreso Alumnos por año",
+                    data: {{ $students_total }},
+                    lineTension: 0,
+                    fill: false,
+                    borderColor: 'orange',
+                    backgroundColor: 'transparent',
+                    borderDash: [5, 5],
+                    pointBorderColor: 'orange',
+                    pointBackgroundColor: 'rgba(255,150,0,0.5)',
+                    pointRadius: 5,
+                    pointHoverRadius: 10,
+                    pointHitRadius: 30,
+                    pointBorderWidth: 2,
+                    pointStyle: 'rectRounded'
+                }]
+            };
+            // var primer_grafico = document.getElementById('grafico').getContext('2d');//seleccionamos el canvas
+            window.line= new Chart(speedCanvas, {
+                type: 'line',
+                data: speedData,
+                options: chartOptions
+            });
+    }
 
-        var speedCanvas = document.getElementById("graficoLineas");
-        var speedData = {
-            labels: {{ $students_dates }},
-            datasets: [{
-                label: "Ingreso Alumnos por año",
-                data: {{ $students_total }},
-                lineTension: 0,
-                fill: false,
-                borderColor: 'orange',
-                backgroundColor: 'transparent',
-                borderDash: [5, 5],
-                pointBorderColor: 'orange',
-                pointBackgroundColor: 'rgba(255,150,0,0.5)',
-                pointRadius: 5,
-                pointHoverRadius: 10,
-                pointHitRadius: 30,
-                pointBorderWidth: 2,
-                pointStyle: 'rectRounded'
-            }]
-        };
-        // var primer_grafico = document.getElementById('grafico').getContext('2d');//seleccionamos el canvas
-        window.line= new Chart(speedCanvas, {
-            type: 'line',
-            data: speedData,
-            options: chartOptions
-        });
 
     </script>
 
