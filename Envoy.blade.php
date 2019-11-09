02acd7da3b2dcb30542c4f7bb1de5f89aedf0798
danielDev <valcyum.085@gmail.com>
2019-11-08 23:07:37 -0400

    envoy

---
+++ Envoy.blade.php
@@ -0,0 +1,14 @@
+@servers( [ 'aws' => 'ubuntu@18.220.190.211'  ] )
+
+@include( 'vendor/autoload.php' )
+
+@task( 'test' )
+echo "tarea test ejecutada"
+@endtask
+
+@task( 'ls', [ 'on' => '$on'] )
+cd /var/www/html
+ls -la
+echo "tarea test ejecutada";
+@endtask
+
