3813316af82f61b613702f0c37c5957e86eeab58
danielDev <valcyum.085@gmail.com>
2020-08-05 16:04:00 -0400

    archivo envoy para subir a aws, falta perfeccionar, o update

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -3,12 +3,7 @@
 @include( 'vendor/autoload.php' )
 
 @task( 'test' )
-echo "tarea test ejecutada"
-@endtask
-
-@task( 'ls', [ 'on' => '$on'] )
-cd /var/www/html
-ls -la
 echo "tarea test ejecutada";
 @endtask
+
 
